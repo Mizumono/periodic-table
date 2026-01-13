783615fa7b59d21523f20fec940a47da47e6858f
Mizumono <arzumanjan.gevorg@gmail.com>
2026-01-13 13:49:53 +0100

    refactor: split lanthanides into separate data file

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,7 @@
 import type { ElementData } from '../types';
 import { alkaliMetals } from './alkaliMetals';
 import { alkalineEarthMetals } from './alkalineEarthMetals';
+import { lanthanides } from './lanthanides';
 import { metalloids } from './metalloids';
 import { nobleGases } from './nobleGases';
 import { postTransitionMetals } from './postTransitionMetals';
@@ -10,6 +11,7 @@ import { transitionMetals } from './transitionMetals';
 export const elements: ElementData[] = [
   ...alkaliMetals,
   ...alkalineEarthMetals,
+  ...lanthanides,
   ...metalloids,
   ...nobleGases,
   ...postTransitionMetals,
