e97d878423802b9762bf5a160ee7f18d6557f269
Mizumono <arzumanjan.gevorg@gmail.com>
2026-01-13 13:23:48 +0100

    refactor: split post-transition metals into separate data file

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,10 +1,12 @@
 import type { ElementData } from '../types';
 import { alkaliMetals } from './alkaliMetals';
 import { alkalineEarthMetals } from './alkalineEarthMetals';
+import { postTransitionMetals } from './postTransitionMetals';
 import { transitionMetals } from './transitionMetals';
 
 export const elements: ElementData[] = [
   ...alkaliMetals,
   ...alkalineEarthMetals,
+  ...postTransitionMetals,
   ...transitionMetals,
 ];
