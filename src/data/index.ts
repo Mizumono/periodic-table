88eecde88f86396cd17307588a9d5b627e2c9202
Mizumono <arzumanjan.gevorg@gmail.com>
2026-01-13 13:29:00 +0100

    refactor: split metalloids into separate data file

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,12 +1,14 @@
 import type { ElementData } from '../types';
 import { alkaliMetals } from './alkaliMetals';
 import { alkalineEarthMetals } from './alkalineEarthMetals';
+import { metalloids } from './metalloids';
 import { postTransitionMetals } from './postTransitionMetals';
 import { transitionMetals } from './transitionMetals';
 
 export const elements: ElementData[] = [
   ...alkaliMetals,
   ...alkalineEarthMetals,
+  ...metalloids,
   ...postTransitionMetals,
   ...transitionMetals,
 ];
---
+++ src/data/metalloids.ts
@@ -0,0 +1,52 @@
+import type { ElementData } from '../types';
+
+export const metalloids: ElementData[] = [
+  {
+    category: 'Metalloid',
+    group: 13,
+    name: 'Boron',
+    number: 5,
+    period: 2,
+    symbol: 'B',
+  },
+  {
+    category: 'Metalloid',
+    group: 14,
+    name: 'Silicon',
+    number: 14,
+    period: 3,
+    symbol: 'Si',
+  },
+  {
+    category: 'Metalloid',
+    group: 14,
+    name: 'Germanium',
+    number: 32,
+    period: 4,
+    symbol: 'Ge',
+  },
+  {
+    category: 'Metalloid',
+    group: 15,
+    name: 'Arsenic',
+    number: 33,
+    period: 4,
+    symbol: 'As',
+  },
+  {
+    category: 'Metalloid',
+    group: 15,
+    name: 'Antimony',
+    number: 51,
+    period: 5,
+    symbol: 'Sb',
+  },
+  {
+    category: 'Metalloid',
+    group: 16,
+    name: 'Tellurium',
+    number: 52,
+    period: 5,
+    symbol: 'Te',
+  },
+];
