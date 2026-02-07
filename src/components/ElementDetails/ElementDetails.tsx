0387e9037ad5364d47f0aab374f4866b3a2766a0
Mizumono <arzumanjan.gevorg@gmail.com>
2026-02-07 15:52:29 +0100

    feat: add physical properties to elements and display them in ElementDetails

--- src/components/ElementDetails/ElementDetails.tsx
+++ src/components/ElementDetails/ElementDetails.tsx
@@ -5,11 +5,25 @@ interface ElementDetailsProps {
   element: ElementData | null;
 }
 
+const renderDetail = (label: string, value: string | number | undefined) => (
+    <>
+      <dt>{label}</dt>
+      <dd>{value}</dd>
+    </>
+);
+
 export const ElementDetails = ({ element }: ElementDetailsProps) => {
   return (
     <div className={styles.dialog}>
       <h2 className={styles.elementName}>{element?.name}</h2>
       <p className={styles.description}>{element?.description}</p>
+      <dl className={styles.detailsList}>
+        {element?.atomicMass && renderDetail('Atomic mass', `${element.atomicMass} u`)}
+        {element?.meltingPoint && renderDetail('Melting', `${element.meltingPoint} °C`)}
+        {element?.boilingPoint && renderDetail('Boiling', `${element.boilingPoint} °C`)}
+        {element?.discoveredBy && renderDetail('Discovered by', element.discoveredBy)}
+        {element?.discoveryYear && renderDetail('Year of discovery', element.discoveryYear)}
+      </dl>
     </div>
   );
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,14 @@
 import type { Category } from './categories';
 
 export interface ElementData {
+  atomicMass?: number;
+  boilingPoint?: number;
   category: Category;
   description?: string;
+  discoveredBy?: string;
+  discoveryYear?: number;
   group: number;
+  meltingPoint?: number;
   name: string;
   number: number;
   period: number;
